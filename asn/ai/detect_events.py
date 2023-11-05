2e613c5d2bfafbc148ee0d6836402f22768bd4c9
zhihao.ouyang <zhihao.ouyang@bytedance.com>
2023-11-05 14:29:11 -0800

    add demo video

--- asn/ai/detect_events.py
+++ asn/ai/detect_events.py
@@ -27,7 +27,7 @@ Please response in json format like:
 ]
 '''
     prompt = ChatPromptTemplate.from_template(prompt_str)
-    # model = ChatOpenAI()
+    # model = ChatOpenAI(model='gpt-3.5-turbo-16k')
     model = ChatOpenAI(model='gpt-4', temperature=0.1)
     chain = prompt | model
     res = chain.invoke({
@@ -50,7 +50,7 @@ def qa_from_video(search_item: str, transcript_text: str=video_transcript_text):
 Response:
 '''
     prompt = ChatPromptTemplate.from_template(prompt_str)
-    model = ChatOpenAI(temperature=0.1)
+    model = ChatOpenAI(temperature=0.1, model='gpt-3.5-turbo-16k')
     # model = ChatOpenAI(model='gpt-4')
     chain = prompt | model
     res = chain.invoke({
@@ -77,7 +77,7 @@ Response:
 [?, ?...]
 '''
     prompt = ChatPromptTemplate.from_template(prompt_str)
-    model = ChatOpenAI(temperature=0.1)
+    model = ChatOpenAI(temperature=0.1, model='gpt-3.5-turbo-16k')
     # model = ChatOpenAI(model='gpt-4')
     chain = prompt | model
     res = chain.invoke({
@@ -88,10 +88,23 @@ Response:
 if __name__ == "__main__":
     # Example usage
     # Generate a Twitter post with background information
-    # item_search1 = "Voter related to revolving funds"
-    # # item_search1 = "Pledge of Allegiance"
-    # post1 = search_event_from_video(item_search1)
-    # print(post1)
+    # item_search1 = "voting for revolving funds"
+    # item_search1 = "Pledge of Allegiance"
+    # item_search1 = "homelessness"
+    item_search1 = "Policy staffing"
+    post1 = search_event_from_video(item_search1)
+    print(post1)
+    '''
+    [
+        {
+            "content": "item 10 an ordinance to amend the administrative code remove the authorization for cash revolving funds and reduce the max amount of the cash revolve ing fund for the port.",
+            "time": "00:06:36",
+            "message": "They are discussing an ordinance to amend the administrative code to remove the authorization for cash revolving funds and reduce the maximum amount of the cash revolving fund for the port."
+        }
+    ]
+
+
+    '''
 
     # item_search2 = "What are they discussing"
     # post2 = qa_from_video(item_search2)
@@ -101,9 +114,9 @@ if __name__ == "__main__":
     # post2 = qa_from_video(item_search2)
     # print(post2)
 
-    item_search2 = "What's the vote of ronen for ?"
-    post2 = qa_from_video(item_search2)
-    print(post2)
+    # item_search2 = "What's the vote of ronen for ?"
+    # post2 = qa_from_video(item_search2)
+    # print(post2)
 
     # has tag test
     # tags = gen_tags_from_video()
